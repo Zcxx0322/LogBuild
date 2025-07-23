3292d33b177965c277088aced1f254de03c8d4b1
Zcxx0322 <3611744356@qq.com>
2025-07-23 14:23:54 +0800

    update phpipam

--- build_win.sh
+++ build_win.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LOG_BUILD_PATH="D:\workspace\LogBuild"
-DEPLOY_PATH="D:\workspace\Zcxx0322.github.io"
+LOG_BUILD_PATH="E:\workspace\LogBuild"
+DEPLOY_PATH="E:\workspace\Zcxx0322.github.io"
 LOG_BUILD_REPO="git@github.com:Zcxx0322/LogBuild.git"
 DEPLOY_REPO="git@github.com:Zcxx0322/Zcxx0322.github.io.git"
 
